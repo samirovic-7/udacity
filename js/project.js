4ca55b6634652946763a048ee1161b5b2c59b97b
samirovic` <77668615+samirovic-7@users.noreply.github.com>
2021-09-22 11:50:57 +0200

    Update project.js

--- js/project.js
+++ js/project.js
@@ -1,8 +1,6 @@
 let li = document.querySelectorAll(".li");
 
 
-// First solution 
-
 let li1 = document.getElementById("li1");
 let li2 = document.getElementById("li2");
 let li3 = document.getElementById("li3");
@@ -10,18 +8,35 @@ let li4 = document.getElementById("li4");
 
 li1.addEventListener("click", function() {
     window.scrollTo(0, 0);
+    li1.classList.add("active");
+    li2.classList.remove("active");
+    li3.classList.remove("active");
+    li4.classList.remove("active");
+
 });
 
 li2.addEventListener("click", function() {
     window.scrollTo(0, 767);
+    li2.classList.add("active");
+    li1.classList.remove("active");
+    li3.classList.remove("active");
+    li4.classList.remove("active");
 
 });
 li3.addEventListener("click", function() {
-    window.scrollTo(0, 1534)
+    window.scrollTo(0, 1534);
+    li3.classList.add("active");
+    li1.classList.remove("active");
+    li2.classList.remove("active");
+    li4.classList.remove("active");
 
 });
 li4.addEventListener("click", function() {
-    window.scrollTo(0, 2301)
+    window.scrollTo(0, 2301);
+    li4.classList.add("active");
+    li1.classList.remove("active");
+    li2.classList.remove("active");
+    li3.classList.remove("active");
 })
 
 
@@ -34,18 +49,25 @@ li4.addEventListener("click", function() {
 
 //     li[0].addEventListener("click", function() {
 //         window.scrollTo(0, 0);
-
+//         item.classList.remove("active");
+//         this.classList.add("active");
 //     });
 //     li[1].addEventListener("click", function() {
 //         window.scrollTo(0, 767);
+//         item.classList.remove("active");
+//         this.classList.add("active");
 
 //     });
 //     li[2].addEventListener("click", function() {
-//         window.scrollTo(0, 1534)
+//         window.scrollTo(0, 1534);
+//         item.classList.remove("active");
+//         this.classList.add("active");
 
 //     });
 //     li[3].addEventListener("click", function() {
-//         window.scrollTo(0, 2301)
+//         window.scrollTo(0, 2301);
+//         item.classList.remove("active");
+//         this.classList.add("active");
 
 //     });
 
